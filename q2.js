80dd7456d990245cb18af2a21268f00bb0264873
Igor <igor.asantosss@outlook.com>
2023-04-27 17:48:57 -0300

    fix(q2): usando piscina para o usar threads paralelas

--- q2.js
+++ q2.js
@@ -1,10 +1,11 @@
-const { Worker } = require('worker_threads');
+const Piscina = require('piscina');
+const path = require('path'); 
 
 const sumMatrix = require('./utils/sum-matrix');
 const generateMatrix  = require('./utils/generate-matrix');
 const separateMatrix  = require('./utils/separate-matrix');
 
-const MATRIX_SIZE = 500;
+const MATRIX_SIZE = 5000;
 
 const matrixA = generateMatrix(MATRIX_SIZE);
 const matrixB = generateMatrix(MATRIX_SIZE);
@@ -18,24 +19,40 @@ sumMatrix(matrixA, matrixB);
 
 const fineshedAt = process.hrtime(startedAt);
 
+const timeWithoutThreads = (fineshedAt[0] * 1e9) + fineshedAt[1];
+console.log("Time without using threads: ", timeWithoutThreads);
 
+
+const pool = new Piscina();
+
+const options = {
+  filename: path.resolve(__dirname, 'thread-q2')
+};
 
 const startedWithThreadsAt = process.hrtime();
 
-submatrixA.forEach((matrix, index) => {
-  const newThread = new Worker('./threads/thread-q2.js', { 
-    workerData: {
-      A: matrix,
-      B: submatrixB[index]
-    }
-  })
-}) 
+(async function() {
+  await Promise.all([
+    pool.run({
+      A: submatrixA[0],
+      B: submatrixB[0],
+    }, options),
+    pool.run({
+      A: submatrixA[1],
+      B: submatrixB[1],
+    }, options),
+    pool.run({
+      A: submatrixA[2],
+      B: submatrixB[2],
+    }, options),
+    pool.run({
+      A: submatrixA[3],
+      B: submatrixB[3],
+    }, options),
+    ])
+})();
 
 const fineshedWithThreadsAt = process.hrtime(startedWithThreadsAt);
 
-
-const timeWithoutThreads = (fineshedAt[0] * 1e9) + fineshedAt[1];
 const timeWithThreads = (fineshedWithThreadsAt[0] * 1e9) + fineshedWithThreadsAt[1];
-
-console.log("Time without using threads: ", timeWithoutThreads);
 console.log("Time with threads: ", timeWithThreads);
